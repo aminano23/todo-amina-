ebeb445127d5077acb2cf742e843c4925e670b5e
aminano23 <urbashova04@gmail.com>
2024-10-31 16:41:59 +0600

    todo

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Counter from "./Counter";
+
+export default function Home() {
+  return (
+    <div>
+      <div>HOME PAGE</div>
+      <Counter/>
+    </div>
+  );
+}
